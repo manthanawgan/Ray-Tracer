7d1483a0a0a7e138a0a73886348f573582de5412
Manthan Awgan <manthanawgan@gmail.com>
2025-11-25 00:36:52 +0530

    gn chat

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main() {
     std::cout << "P3 \n" << image_width << ' ' << image_height << "\n 255 \n";
 
     for (int j = 0; j < image_height; j++) {
+        std::cout << "\r Scanlines remaining : " << (image_height - j) << " " << std::flush; 
         for (int i = 0; i < image_width; i++){
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -22,8 +23,8 @@ int main() {
 
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
+
+        std::cout << "\r Done.                           \n";
     }
-
-
 
 }
